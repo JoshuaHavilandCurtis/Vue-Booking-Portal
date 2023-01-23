619e0976f1ec9fdb30057476ac64e7ef9036bd2d
Joshua Curtis <josh.curtis@greenwoodcampbell.com>
2023-01-23 12:12:24 +0000

    New components - hourlyintervals

---
+++ src/components/HourlyIntervals.vue
@@ -0,0 +1,25 @@
+<template>
+    <container>
+        <div class="intervals">
+            <ul>
+                <li>
+                    <span class="intervals__time">11:10am</span>
+                    <button>Book Appointment</button>
+                </li>
+            </ul>
+        </div>
+    </container>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		
+        }
+    },
+    mounted() {
+		this.$store.commit("markRouteAsLoaded");
+	}
+}
+</script>
---
+++ src/views/SelectTime.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="fadein my-3">
+        <hourly-intervals></hourly-intervals>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import apiConfig from "../api.config";
+
+export default {
+	methods: {
+        async getHourlyIntervals() {
+
+            try {
+                const response = await axios.get(apiConfig.getHourlyIntervals);
+			    const apiIntervals = response.data.entries;
+			    return apiIntervals;
+            } catch (error) {
+                console.error(error)
+            }
+		},
+	},
+	created() {
+		this.getHourlyIntervals();
+	},
+	beforeCreate() {
+		this.$store.state.request = null;
+	}
+}
+</script>
